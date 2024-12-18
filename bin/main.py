a70602df2e4c6fff5bc747f4d7b95a815911c613
Mark Krautheim <mark.krautheim@gmail.com>
2024-12-18 09:09:38 -0500

    Add more, for fun

--- bin/main.py
+++ bin/main.py
@@ -8,7 +8,8 @@ NOTE: the letters must be given in this order:
 2. Proceed clockwise around the square.
 """
 
-from itertools import combinations, permutations
+from collections import Counter
+from itertools import chain, combinations, permutations
 from pathlib import Path
 import re
 import sys
@@ -17,27 +18,28 @@ WORDLIST = "/home/mark/word.list"
 # WORDLIST = "/home/mark/nwl2023_words.list"
 WORDS = Path(WORDLIST).read_text().splitlines()
 
-# Remove words with doubled letters
-fwords = [word for word in WORDS if not re.search(r"([a-z])\1", word)]
+# Remove words with doubled letters, as these can't be played
+words = [word for word in WORDS if not re.search(r"([a-z])\1", word)]
 
 # Get command-line arguments
 letters, nonwords = list(sys.argv[1]), sys.argv[2:]
 
 # Find all the words that use only the given letters
 pat = re.compile(f"^[{''.join(letters)}]+$")
-matches = [w for w in fwords if pat.search(w)]
+words = [w for w in words if pat.search(w)]
 
 # Remove words that have sequences of letters
 # that appear on the same side of the square.
 sides = [letters[0:3], letters[3:6], letters[6:9], letters[9:12]]
 patstr = ["|".join(["".join(l) for l in permutations(side, 2)]) for side in sides]
 pat = re.compile("|".join(patstr))
-matches = [word for word in matches if not pat.search(word)]
+words = [word for word in words if not pat.search(word)]
 
-matches = [word for word in matches if word not in nonwords]
+# Remove words not recognized by the NYT editor
+words = [word for word in words if word not in nonwords]
 
 # Find the 2-word combinations that use all the given letters
-combos = [(w1, w2) for w1, w2 in combinations(matches, 2) if len(set(w1 + w2)) == 12]
+combos = [(w1, w2) for w1, w2 in combinations(words, 2) if len(set(w1 + w2)) == 12]
 
 # Filter to just the word sequences with connecting last/first letters
 solutions = []
@@ -49,8 +51,9 @@ for w1, w2 in combos:
 
 # Print the solutions. You'll have to try questionable words
 # manually in the puzzle to see if they're "valid".
+print("Solutions sorted by letter length")
 for s in sorted(solutions, key=lambda s: len(s[0] + s[1]), reverse=True):
-    print(s)
+    print(f"{s!r:30s} {len(s[0] + s[1]):2d}")
 
 # Find the 'pencil-line' length for each solution
 coords = (
@@ -71,8 +74,10 @@ coords = (
 lettercoords = {l: c for l, c in zip(letters, coords)}
 
 totaldists = {}
+traces = []
 for s in solutions:
     trace = s[0] + s[1][1:]
+    traces.append(trace)
     totaldist = 0
     for l0, l1 in zip(trace[0:-1], trace[1:]):
         c0x, c0y = lettercoords[l0]
@@ -80,3 +85,21 @@ for s in solutions:
         d = ((c1x - c0x) ** 2 + (c1y - c0y) ** 2) ** 0.5
         totaldist += d
     totaldists[s] = totaldist
+
+totaldists = {
+    sol: pld
+    for sol, pld in sorted(totaldists.items(), key=lambda item: item[1], reverse=True)
+}
+
+print("\nSolutions sorted by pencil-line distance")
+for sol, pld in totaldists.items():
+    print(f"{sol!r:30s} {pld:5.2f}")
+
+# Get a list of the unique words used in all the solutions
+flatsols = list(chain(*solutions))
+
+# Get a count of all the 2-letter combinations in the solutions
+lettercombos = Counter()
+for t in traces:
+    for l0, l1 in zip(t[0:-1], t[1:]):
+        lettercombos[(l0, l1)] += 1
